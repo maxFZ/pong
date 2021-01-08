36c0b0bd4fcd61be10bb1df924c4e2b17dc04e73
maxFZ <maksym2234@gmail.com>
2021-01-08 20:06:45 +0200

    add scoreboard

--- ball.py
+++ ball.py
@@ -8,6 +8,7 @@ class Ball(Turtle):
         self.y_move = 10
         self.x_move = 10
         self.penup()
+        self.move_speed = 0.1
 
 
     def move(self):
@@ -19,5 +20,12 @@ class Ball(Turtle):
     def bounce_y(self):
         self.y_move *= - 1
 
+
     def bounce_x(self):
         self.x_move *= - 1
+        self.move_speed *= 0.9
+        
+    def reset_position(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.bounce_x()
